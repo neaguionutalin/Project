6c1941d8deb39dca769550a7a8deb2aa1086cea5
Ionut Alin Neagu <neagu_ionutalin@icloud.com>
2018-04-19 12:27:03 +0100

    interface v2

--- Client.java
+++ Client.java
@@ -2,8 +2,8 @@ package com.m3c.ane;
 
 public interface Client {
     int getClientId();
-    void sendOrder(Object o);
-    void setBalance(double balance);
-    double getBalance(int clientId);
-    void cancelOrder(Object o);
+    void sendClientOrder(Object o);
+    void setClientBalance(double balance);
+    double getclientBalance(int clientId);
+    void cancelClientOrder(Object o);
 }
